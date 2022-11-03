c7f850145da5ed887fd633405241aacfc3720883
Striker2785 <64331077+Striker2785@users.noreply.github.com>
2022-11-03 16:38:17 -0600

    Update Move.h

--- Move.h
+++ Move.h
@@ -8,12 +8,6 @@
 class Piece;
 class Board;
 
-/**
- * Represents a single move of a piece.
- * 
- * @author Mckenna Cisler
- * @version 5.18.2015
- */
 class Move
 {
 	private:
@@ -23,33 +17,23 @@ class Move
     
     public:    
 		/**
-		 * Constructor for objects of class Move - initializes starting and final position.
-		 * @param x1 Starting x position.
-		 * @param y1 Starting y position.
-		 * @param x2 Ending x position.
-		 * @param y2 Ending y position.
-		 * @param precedingMove The move preceding this one (can be null if move is first)
+		 * @param x1 Posicion inicial x
+		 * @param y1 Posicion incial y
+		 * @param x2 Posicion final x
+		 * @param y2 Posicion final y
+		 * @param precedingMove
 		 */
 		Move(int x1, int y1, int x2, int y2, const move_ptr_t precedingMove, bool isJump) :
 			x1(x1), y1(y1), x2(x2), y2(y2), precedingMove(precedingMove), isJump(isJump)
 			{};
 
-		/**
-		 * @return Returns a two-part array representing the coordinates of this move's starting position.
-		 */
+		
 		coords_t getStartingPosition() const;
 		
-		/**
-		 * @return Returns a two-part array representing the coordinates of this move's ending position.
-		 */
+
 		coords_t getEndingPosition() const;
 		
-		/**
-		 * Finds the pieces jumped in this move.
-		 * (Get's inbetween jumps using recursion)
-		 * @return Returns an array of pieces that were jumped.
-		 * @param board The board to look for the pieces on.
-		 */
+		
 		std::vector<Piece*> getJumpedPieces(const Board& board) const;
 };
 
