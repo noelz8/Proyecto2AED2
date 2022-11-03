41855ca7ee3ea25d21abee3bf30c71e462fb747c
Striker2785 <64331077+Striker2785@users.noreply.github.com>
2022-11-03 16:54:24 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -4,18 +4,14 @@
 class Board;
 
 /**
- * An abstract version of a player, from which Human and AI Players will be extended.
- * Used so that both player types can be used interchangably.
- * 
- * @author Mckenna Cisler
- * @version 5.18.2016
+ * Una version abstracta del jugador
  */
 class Player
 {
 	public:
 		/**
-		 * Gets a move, by asking the given player what move they want to do.
-		 * @param board The board to apply the move to
+		 * Obtiene un moviemto preguntadole al jugador que quiere hacer
+		 * @param board El tablero
 		 */
 		virtual void getMove(Board& board) = 0;
 };
